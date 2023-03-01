c52c8c5d2894185b7d64a61af812f90498420aca
Daniel Cochico <dcochico.20@gmail.com>
2023-03-01 13:22:56 -0800

    Add places

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Rachel","Monica","Chandler","Ross","Phoebe"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["Colombia","Italy","Iceland","Germany","Switzerland"]
+console.log(places.push("San Diego"))
